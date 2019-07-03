bb84e3d8700208671984c6df2f4f73b2be8f0664
Will Worth <will.worth@gmail.com>
2019-07-03 18:43:12 +0200

    style

--- src/DogDetails.js
+++ src/DogDetails.js
@@ -1,11 +1,36 @@
 import React, { Component } from "react";
-
+import "./DogDetails.css";
+import { Link } from "react-router-dom";
 export default class DogDetails extends Component {
   render() {
+    let { dog } = this.props;
     return (
-      <div>
-        <h1>dog detail</h1>
-        <h2>{this.props.dog.name}</h2>
+      <div className="container">
+        <div className="DogDetails row justify-content-center mt-5">
+          <div className="col-11 col-lg-5">
+            <div className="DogDetails-card card">
+              <img className="card-img-top" src={dog.src} alt={dog.name} />
+              <div className="card-body">
+                <h2 className="card-title">{dog.name}</h2>
+                <h4 className="card-subtitle text-muted">
+                  {dog.age} years old
+                </h4>
+              </div>
+              <ul className="list-group list-group-flush">
+                {dog.facts.map((fact, i) => (
+                  <li className="list-group-item" key={i}>
+                    {fact}
+                  </li>
+                ))}
+              </ul>
+              <div className="card-body">
+                <Link className="btn btn-info" to="/dogs">
+                  Go back
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
